1850bc70f692eecbf224606c3a7b2a2c9e153411
mattsky <matt.northsky@gmail.com>
2019-07-28 14:36:13 -0700

    EPG programme parsing functional

--- epg_test_stuff/epg_test_parser.py
+++ epg_test_stuff/epg_test_parser.py
@@ -11,7 +11,7 @@ class EPG_Parser():
         self.tree = ET.parse('xmltv.xml')
         self.root = self.tree.getroot()
 
-    def epg_chunker(self):
+    def epg_channel_chunker(self):
         #tree = ET.parse(xmldata)
         print(self.root)
         for p in self.root.findall('.//channel'):
@@ -26,5 +26,30 @@ class EPG_Parser():
             #</channel>
             print("%s" % (p.find('display-name').text))
             print(p.get('id'))
-            for q in p.findall('.//icon'):
-                print(q.get('src'))
+            #for q in p.findall('.//icon'):
+                #print(q.get('src'))
+            try: 
+                print(p.find('.//icon').get('src'))
+            except:
+                pass
+
+
+    def epg_programme_chunker(self):
+
+        for p in self.root.findall('.//programme'):
+            #<programme start="20190729043000 -0400" stop="20190729044500 -0400" channel="cartoonnetwork.us" >
+            # <title>Lazor Wulf</title>
+            # <desc>Lazor Wulf decides he is just going to starve to death when his favorite food place gets wrecked. Stupid Horse tries to recreate the menu because he&apos;s good like that.</desc>
+            # </programme>
+            #programme
+            #title
+            #desc
+            #programme
+            print(p)
+            #for elem in p.iter():
+            #    print(elem.tag)
+            #print(p.tag)
+            print(p.attrib)
+            print(p.find('.//title').text)
+            print(p.find('.//desc').text)
+            
